6ba0b7349a6c515eb46d216723fbfc506757a98d
dhruti <dhruti@DESKTOP-4S1SR41>
2016-07-22 10:41:01 +0100

    airline created

---
+++ airlines/Airline.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline
+{
+	public class Airline
+	{
+		//Private Properties +++++++++++++++++++++++++++
+		List<String> _seatingChart = new List<string> ();
+		List<String> _assignedChart = new List<string> ();
+
+		private string change;
+		//Public Properties ++++++++++++++++++++++++++++
+		public Airline()
+		{
+			_seatingChart.Add ("First");
+			_seatingChart.Add ("First");
+			_seatingChart.Add ("First");
+			_seatingChart.Add ("First");
+			_seatingChart.Add ("First");
+			_seatingChart.Add ("Economy");
+			_seatingChart.Add ("Economy");
+			_seatingChart.Add ("Economy");
+			_seatingChart.Add ("Economy");
+			_seatingChart.Add ("Economy");
+		}
+
+		//Public Methods +++++++++++++++++++++++++++++++
+		public void reserveFirstClass()
+		{
+			int seat = 0;
+			while (seat < 5) {
+				if (_seatingChart [seat] == "First") {
+					_assignedChart.Add (_seatingChart [seat]);
+					_seatingChart.RemoveAt (seat);
+					this.print();
+					break;
+				} else{
+					Console.WriteLine ("First class is fully booked");
+				}
+				seat++;
+			}
+		}
+
+		public void reserveEconomicClass()
+		{
+			int seat = 6;
+			while (seat > 5 && seat < _seatingChart.Count) 
+			{
+				if (_seatingChart [seat] == "Economy") 
+				{
+					_assignedChart.Add (_seatingChart [seat]);
+					_seatingChart.RemoveAt (seat);
+					this.print();
+					break;
+				} else 
+				{
+					Console.WriteLine ("Economy class is fully booked");
+					Console.WriteLine ("Would you like to book your seat in the First Class?/n 1. Press y for Yes /n 2. Press n for No");
+					change = Console.ReadLine ().ToLower ();
+				}
+				if (change == "y") 
+				{
+					reserveFirstClass ();
+				} else 
+				{
+					Console.WriteLine ("Next Flight leaves in 3 hours");
+				}
+				seat++;
+			}
+		}
+		public void print()
+		{
+			Console.WriteLine ("Congratulations! Your Seat is Booked");
+		}
+		public void Exit()
+		{
+			Console.Clear();
+		}
+	}
+}
--- airlines/Program.cs
+++ airlines/Program.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Airline;
 
-namespace airlines
+namespace Airline
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int choice = 0;
+			Airline airline = new Airline();
+			while(choice != 3)
+			{
+				Console.WriteLine("1. Press 1 For First Class");
+				Console.WriteLine("2. Press 2 For Economy Class");
+				Console.WriteLine("3. Press 3 For Exit");
+				try
+				{
+					choice = Convert.ToInt32(Console.ReadLine());
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+					choice = 0;
+				}
+				switch (choice)
+				{
+				case 1: airline.reserveFirstClass();
+					break;
+
+				case 2: airline.reserveEconomicClass();
+					break;
+				case 3: airline.Exit();
+					break;
+				default:
+					Console.WriteLine("Make proper selection");
+					break;
+				}
+			}
+		}
+	}
 }
